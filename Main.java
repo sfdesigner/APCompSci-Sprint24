79bd1ae85280f5487685fa8d3f329b189c0102f0
Doug <doug@sfdesignerdw.com>
2018-10-05 19:10:48 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        displayMsg();
+    }
+
+    public static void displayMsg() {
+        System.out.println("This is within the myFunction function");
+    }
+
+}
